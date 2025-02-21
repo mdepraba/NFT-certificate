6c188032d62309c7a669c3e84fe99ac317cd5543
Praba Kusuma <prabakusuma764@gmail.com>
2025-02-21 21:14:33 +0800

    init commit

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {ERC721, ERC721URIStorage} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
+contract Counter is ERC721{
+    constructor() ERC721("Made Praba Jaya Kusuma", "MPJK") {}
 }
